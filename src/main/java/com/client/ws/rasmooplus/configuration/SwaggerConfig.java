ec4e46c455f3e32bf0354afea2a1cb30c1fdf744
GabrielCrespo <gabriel.crespo1942@gmail.com>
2024-08-19 20:39:07 -0300

    feature: adicinando swagger para spring 3 com OpenAPI

--- src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
+++ src/main/java/com/client/ws/rasmooplus/configuration/SwaggerConfig.java
@@ -1,39 +1,50 @@
 package com.client.ws.rasmooplus.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .useDefaultResponseMessages(false)
-//                .groupName("v0")
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.client.ws.rasmooplus.controller"))
-//                .paths(PathSelectors.any())
-//                .build().apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//
-//        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-//
-//        return apiInfoBuilder
-//                .title("Rasmoo Plus")
-//                .description("Api para atender o Rasmoo Plus")
-//                .version("0.0.1")
-//                .license("Rasmoo cursos de tecnologia")
-//                .build();
-//    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(info()).servers(List.of(server()));
+    }
+
+    private Server server() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8082");
+        server.setDescription("Servidor");
+        return server;
+    }
+
+    private Contact contact() {
+        Contact contact = new Contact();
+        contact.setEmail("rasmoo@email.com");
+        contact.setName("Rasmoo");
+        contact.setUrl("https://www.rasmoo.com");
+        return contact;
+    }
+
+    private License license() {
+        return new License().name("MIT License")
+                .url("https://choosealicense.com/licenses/mit/");
+    }
+
+    private Info info() {
+        return new Info().title("Api rasmooplus")
+                .version("1.0")
+                .contact(contact())
+                .description("Essa Api expõe os endepoints para gerenciar o rasmooplus")
+                .termsOfService("https://www.rasmoo.com/terms")
+                .license(license());
+    }
+
 }
